959824ce9f0b518eac0fa80d6a9e7e8f7da07674
avinasmishra <avinash.mishra0695@gmail.com>
2023-12-07 10:08:39 +0530

    created java file in person Y

---
+++ src/Assignment/GitPactiseY.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class GitPactiseY {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Assignment4");
+		System.out.println("Assignment5");
+		System.out.println("Assignment6");
+
+	}
+
+}
