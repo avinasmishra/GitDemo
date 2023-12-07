b7af52f39b3f399d9b0f9cb2ab5095953b88b042
avinasmishra <avinash.mishra0695@gmail.com>
2023-12-07 09:33:02 +0530

    created java file in person X

---
+++ src/Assignment/GitPactiseX.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class GitPactiseX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Assignment1");
+		System.out.println("Assignment2");
+		System.out.println("Assignment3");
+
+	}
+
+}
